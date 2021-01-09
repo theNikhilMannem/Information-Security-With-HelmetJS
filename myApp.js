14cfa458568b6c77f38425708adb8a9f16b3bbf1
repl.it user <replituser@example.com>
2021-01-09 12:11:07 +0000

    commit after finishing all the challenges

--- myApp.js
+++ myApp.js
@@ -1,28 +1,44 @@
 var express = require('express');
 var app = express();
 
+const helmet = require('helmet')
 
+// app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }))
 
+// app.use(helmet.frameguard({ action: 'deny' }))
 
+// app.use(helmet.xssFilter())
 
+// app.use(helmet.noSniff())
 
+// app.use(helmet.ieNoOpen())
 
+// const ninetyDaysInSeconds = 90*24*60*60
+// app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }))
 
+// app.use(helmet.dnsPrefetchControl())
 
+// app.use(helmet.noCache())
 
+// app.use(helmet.contentSecurityPolicy({ 
+//   "directives": {
+//     defaultSrc: ["'self'"],
+//     scriptSrc: ["'self'", "trusted-cdn.com"]
+//   }
+// }))
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet({
+  frameguard: {
+    action: 'deny'
+  },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["style.com"]
+    }
+  },
+  dnsPrefetchControl: false
+}))
 
 
 
